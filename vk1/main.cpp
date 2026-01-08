2bab70bd1bf207f8f80c58158c30fa025cb19a0a
eliastikanoja <elias.tikanoja@gmail.com>
2026-01-08 09:59:43 +0200

    harjoootus

--- vk1/main.cpp
+++ vk1/main.cpp
@@ -2,8 +2,34 @@
 
 using namespace std;
 
+// funktion prototyyppi
+double keskiarvo(double taulukko[4]);
 int main()
 {
-    cout << "moro" << endl;
+    /*
+    int num = 123;
+    float pi = 3.14;
+    string hello = "Hello Pi: " ;
+
+    double luvut[4];
+
+    cout << hello << pi << " " << num << endl;
+    cout << "Hello:n pituus " << hello.length() << endl;
+*/
+    double luvut[4] = {8.34, 63.34, 11.0, 5.6};
+    double ka = keskiarvo(luvut);
+    cout << "keskiarvo on " << ka << endl;
+    cout << "keskiarvo on " << keskiarvo(luvut) << endl;
+
     return 0;
+}
+
+double keskiarvo (double taulukko[4])
+{
+    double tulos = 0.0;
+    for (int i=0; i < 4; i++) {
+        tulos += taulukko[i];
+    }
+    tulos = tulos / 4.0;
+    return tulos;
 }
