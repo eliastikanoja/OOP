e37980ceeca46d4b31ab8214e258522bc6391064
eliastikanoja <elias.tikanoja@gmail.com>
2026-01-12 16:35:40 +0200

    Valmis viikkotehtävä 1

--- vk1/main.cpp
+++ vk1/main.cpp
@@ -30,7 +30,7 @@ satunnaisluku = rand() % maxnum;
 while (true) {
 
     //kysytään pellaajalta lukua
-    cout << "Anna luku? "
+    cout << "Arvaa luku? "
          << endl;
     cin >> arvaus;
     arvausten_lkm++;
@@ -57,33 +57,3 @@ while (true) {
 }
 return arvausten_lkm;
 }
-    /*
-    int num = 123;
-    float pi = 3.14;
-    string hello = "Hello Pi: " ;
-
-    double luvut[4];
-
-    cout << hello << pi << " " << num << endl;
-    cout << "Hello:n pituus " << hello.length() << endl;
-
-    double luvut[4] = {8.34, 63.34, 11.0, 5.6};
-    double ka = keskiarvo(luvut);
-    cout << "keskiarvo on " << ka << endl;
-    cout << "keskiarvo on " << keskiarvo(luvut) << endl;
-
-    return 0;
-}
-
-double keskiarvo (double taulukko[4])
-{
-    double tulos = 0.0;
-    for (int i=0; i < 4; i++) {
-        tulos += taulukko[i];
-    }
-    tulos = tulos / 4.0;
-    return tulos;
-}
-*/
-
-
