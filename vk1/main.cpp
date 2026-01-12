e56d0ad41eb9f05c59719591be45d4bb185e7a6a
eliastikanoja <elias.tikanoja@gmail.com>
2026-01-12 16:34:16 +0200

    Valmis viikkotehtävä 1

--- vk1/main.cpp
+++ vk1/main.cpp
@@ -3,9 +3,60 @@
 using namespace std;
 
 // funktion prototyyppi
-double keskiarvo(double taulukko[4]);
+int game(int maxnum);
 int main()
 {
+    int arvausten_lkm = 0;
+    int maxnum = 40;
+    arvausten_lkm = game(maxnum);
+    cout << "Arvauksia: "
+         << arvausten_lkm
+         << endl
+        ;
+    return 0;
+}
+
+int game (int maxnum) {
+    int arvausten_lkm = 0;
+    int satunnaisluku = 0;
+    int arvaus = 0;
+     cout << "Maxnum="
+          << maxnum
+          << endl;
+
+     // Arvotaan satunnainen luku
+     srand(time(NULL));
+satunnaisluku = rand() % maxnum;
+while (true) {
+
+    //kysytään pellaajalta lukua
+    cout << "Anna luku? "
+         << endl;
+    cin >> arvaus;
+    arvausten_lkm++;
+
+    //tarkistetaan arvaus=jos se on yhtäsuuri kuin kokonaisluku
+    if (arvaus == satunnaisluku) {
+        cout << "Oikea arvaus!"
+             << endl;
+    break;
+}
+
+    //tarkistetaan arvaus=jos se on on pienempi kuin kokonaisluku
+    if (arvaus < satunnaisluku) {
+        cout << "Luku on suurempi"
+             << endl;
+    }
+
+    //tarkistetaan arvaus=jos se on suurempi kuin kokonaisluku
+    if (arvaus > satunnaisluku) {
+        cout << "Luku on pienempi"
+             << endl;
+    }
+
+}
+return arvausten_lkm;
+}
     /*
     int num = 123;
     float pi = 3.14;
@@ -15,7 +66,7 @@ int main()
 
     cout << hello << pi << " " << num << endl;
     cout << "Hello:n pituus " << hello.length() << endl;
-*/
+
     double luvut[4] = {8.34, 63.34, 11.0, 5.6};
     double ka = keskiarvo(luvut);
     cout << "keskiarvo on " << ka << endl;
@@ -33,3 +84,6 @@ double keskiarvo (double taulukko[4])
     tulos = tulos / 4.0;
     return tulos;
 }
+*/
+
+
